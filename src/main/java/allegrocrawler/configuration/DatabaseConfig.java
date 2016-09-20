107112d39cee9523c039f0b4b2b11e4d5d13d0f0
demageron <demageron@gmail.com>
2016-09-20 23:06:59 +0200

    Datasource refactoring

--- src/main/java/allegrocrawler/configuration/DatabaseConfig.java
+++ src/main/java/allegrocrawler/configuration/DatabaseConfig.java
@@ -1,22 +1,33 @@
 package allegrocrawler.configuration;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by demageron on 20.09.16.
  */
 @Configuration
 public class DatabaseConfig {
+
     @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DataSourceBuilder
+                .create()
+                .username(username)
+                .password(password)
+                .url(dbUrl)
+                .build();
     }
+
 }
